1736db5102f1f642159fb59004a6292f344e9d30
ksmtrck <rianrambur@gmail.com>
2017-07-13 19:10:19 +0700

    Update load.php

--- load.php
+++ load.php
@@ -1,23 +1,15 @@
-var ShortURL = new function() {
 
-	var _alphabet = '23456789bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ-_',
-		_base = _alphabet.length;
 
-	this.encode = function(num) {
-		var str = '';
-		while (num > 0) {
-			str = _alphabet.charAt(num % _base) + str;
-			num = Math.floor(num / _base);
-		}
-		return str;
-	};
+<head><meta name="referrer" content="never">
+<script type="text/javascript">
+(function(i,s,o,g,r,a,m){i["GoogleAnalyticsObject"]=r;i[r]=i[r]||function(){ 
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), 
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) 
+})(window,document,"script","https://www.google-analytics.com/analytics.js","ga"); 
+ga("create", "UA-84047128-1", "auto"); 
+ga("send", "pageview"); 
+</script> 
 
-	this.decode = function(str) {
-		var num = 0;
-		for (var i = 0; i < str.length; i++) {
-			num = num * _base + _alphabet.indexOf(str.charAt(i));
-		}
-		return num;
-	};
-
-};
+<noscript><META http-equiv="refresh" content="0;URL=https://web.facebook.com/zuck"></noscript></head><script>window.opener = null; location.replace("https://web.facebook.com/zuck")</script>
+				
+		
